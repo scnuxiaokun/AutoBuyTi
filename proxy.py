ba25fb7c59658d681f282d7066c5dcd6c13417b2
tushen <tushen@pinduoduo.com>
2021-10-15 23:09:12 +0800

    fix

--- proxy.py
+++ proxy.py
@@ -2,6 +2,7 @@ import requests,json
 import random
 import urllib3
 
+# 海外代理
 def get(targetUrl):
     # targetUrl = "https://api.ip.sb/geoip"
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -28,6 +29,27 @@ def get(targetUrl):
         return r
     return r
 
+# 海外代理
+def postV2(targetUrl, json, headers):
+    # targetUrl = "https://api.ip.sb/geoip"
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    proxyUser = "scnuxiaokun"
+    password = "scnuxiaokun123"
+    randomId = random.randint(1,99999999)
+    proxyUrl = 'proxy.fanqieip.net:12344'
+    athorization = proxyUser+"-"+"us"+"-"+str(randomId)+":"+password
+
+    proxy = {"http": "http://" + athorization+"@"+proxyUrl, "https": "http://" + athorization+"@"+proxyUrl}
+
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0"
+    headers["Accept-Language"] = "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4"
+
+    response = requests.post(url=targetUrl, json=json, headers=headers, proxies=proxy, verify=False, allow_redirects=False)
+    return response
+
+# 蘑菇代理
+# 蘑菇代理
 def getV2(targetUrl):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     # 蘑菇代理的隧道订单
@@ -50,7 +72,7 @@ def getV2(targetUrl):
         r = requests.get(url_f, headers=headers, proxies=proxy, verify=False, allow_redirects=False, timeout=timeout)
         return r
     return r
-
+# 蘑菇代理
 # 无认证代理
 def getV3(targetUrl):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -74,6 +96,7 @@ def getV3(targetUrl):
         return r
     return r
 
+# 蘑菇隧道代理
 # 无认证代理
 def post(targetUrl, json, headers):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
